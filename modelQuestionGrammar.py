4e71b1dcfa565c378033df101a8b95a157be8c21
Casper <casper.prinsen@gmail.com>
2025-10-16 14:26:34 +0200

    update

--- modelQuestionGrammar.py
+++ modelQuestionGrammar.py
@@ -108,33 +108,34 @@ parsetrees(l_spEx,experiments)
 l_questions = Lark(questionGrammar  + footer
         ,parser='earley', start='question', keep_all_tokens=True)
 testquestions=[
-'What should be the location for each windmill of windfarm now so that the sum of amount of (energy for each windmill) for windfarm will be maximal in the future?',
-'What would be the sum of the amount of (capacity of the amount of ethanol for each producer) for "Brazil" in 2030 if the proportional amount of (tax of amount of ethanol for each consumer) of "Brazil" was equal to 1.23 R/l from now on ?',
-'What would be the averaged amount of (ethanol for each consumer) for "Brazil" in 2030 if the proportional amount of (tax of amount of ethanol for each consumer) of "Brazil" was equal to 1.23 R/l from now on?',
-'What could have been the density of amount of lifestock for each location in "Sudan" 10.000 years ago given that the landcover for each location in "Sudan" was equal to "arid land" at the end of the African humid period?',
-'What is the proportional amount of space of green for each neighborhood in "Amsterdam" now?',
-'What would be the proportional amount of space of green for each neighborhood in "Amsterdam" in the future if the quantified amount of building was such and such now?',
-'What should be the proportional amount of space of green for each neighborhood in "Amsterdam" now such that the quantified amount of health for each inhabitant will be such and such in the future?',
+"What should be the amount of green for each neighborhood in 'Amsterdam' now so that the proportional amount of NO2 will be below 20 µg/m³ this summer?"
+#'What should be the location for each windmill of windfarm now so that the sum of amount of (energy for each windmill) for windfarm will be maximal in the future?',
+#'What would be the sum of the amount of (capacity of the amount of ethanol for each producer) for "Brazil" in 2030 if the proportional amount of (tax of amount of ethanol for each consumer) of "Brazil" was equal to 1.23 R/l from now on ?',
+#'What would be the averaged amount of (ethanol for each consumer) for "Brazil" in 2030 if the proportional amount of (tax of amount of ethanol for each consumer) of "Brazil" was equal to 1.23 R/l from now on?',
+#'What could have been the density of amount of lifestock for each location in "Sudan" 10.000 years ago given that the landcover for each location in "Sudan" was equal to "arid land" at the end of the African humid period?',
+#'What is the proportional amount of space of green for each neighborhood in "Amsterdam" now?',
+#'What would be the proportional amount of space of green for each neighborhood in "Amsterdam" in the future if the quantified amount of building was such and such now?',
+#'What should be the proportional amount of space of green for each neighborhood in "Amsterdam" now such that the quantified amount of health for each inhabitant will be such and such in the future?',
 ]
 #These are the questions used in the paper:
 questions =[
-'What is the quantified amount of time to ambulance station with minimal quantified amount of time from each building in "Rotterdam" at present?',
+#'What is the quantified amount of time to ambulance station with minimal quantified amount of time from each building in "Rotterdam" at present?',
 
-'What is the temperature for each location in "Utrecht" now given that the temperature for each sensor location is such and such now?',
-'What will be the temperature in "Utrecht" tomorrow given that the temperature in "Utrecht" is 5 C today?',
-'What could have been the event in "Utrecht" yesterday causing the proportional amount of water for soil in "Utrecht" being 0.3 today?',
-'What would be the temperature in "Utrecht" in 20 years if the proportional amount of CO2 emissions of the world’s economy were halved today?',
-'What should be the amount of green in "Utrecht" today so that the maximum temperature in "Utrecht" will be below 30 C this summer?',
+#'What is the temperature for each location in "Utrecht" now given that the temperature for each sensor location is such and such now?',
+#'What will be the temperature in "Utrecht" tomorrow given that the temperature in "Utrecht" is 5 C today?',
+#'What could have been the event in "Utrecht" yesterday causing the proportional amount of water for soil in "Utrecht" being 0.3 today?',
+#'What would be the temperature in "Utrecht" in 20 years if the proportional amount of CO2 emissions of the world’s economy were halved today?',
+#'What should be the amount of green in "Utrecht" today so that the maximum temperature in "Utrecht" will be below 30 C this summer?',
 
-'What is the closest ambulance station for each building in "Rotterdam" at present given that the location of each ambulance station is such and such now?',
-'What will be the time from the closest ambulance station to each building in "Rotterdam" from now on given that the location of each ambulance station is such and such now?',
-'What will be the sum of the amount of rain for each location in "Dortmund" tomorrow given that the air pressure and temperature for each location in "Germany" is such and such now?',
-'What could have been the event for each road intersection pair in "Rotterdam" last week causing the travel time of this ambulance station to this road accident in "Rotterdam" to be 30 minutes now?',
-'What could have been the route for each language group in "the Amazon" starting 15.000 years ago causing the location of language groups to be such and such at present?',
-'What would be the travel time from the closest ambulance station to each building in "Rotterdam" in the future if the travel time between this road intersection pair was infinite minutes from now on?',
-'What would be the sum of the production of ethanol for each producer in "Brazil" in 2030 if the proportional amount of tax for ethanol for each consumer in "Brazil" was equal to 1.23 R/l from now on',
-'What should be the location of ambulance stations in "Rotterdam" now such that the travel time to each building from the closest ambulance station will be less than 14 minutes in the future?',
-'What should be the location for each windmill of this windfarm now so that the sum of the amount of energy for each windmill of this windfarm will be maximal in the future?'
+#'What is the closest ambulance station for each building in "Rotterdam" at present given that the location of each ambulance station is such and such now?',
+#'What will be the time from the closest ambulance station to each building in "Rotterdam" from now on given that the location of each ambulance station is such and such now?',
+#'What will be the sum of the amount of rain for each location in "Dortmund" tomorrow given that the air pressure and temperature for each location in "Germany" is such and such now?',
+#'What could have been the event for each road intersection pair in "Rotterdam" last week causing the travel time of this ambulance station to this road accident in "Rotterdam" to be 30 minutes now?',
+#'What could have been the route for each language group in "the Amazon" starting 15.000 years ago causing the location of language groups to be such and such at present?',
+#'What would be the travel time from the closest ambulance station to each building in "Rotterdam" in the future if the travel time between this road intersection pair was infinite minutes from now on?',
+#'What would be the sum of the production of ethanol for each producer in "Brazil" in 2030 if the proportional amount of tax for ethanol for each consumer in "Brazil" was equal to 1.23 R/l from now on',
+#'What should be the location of ambulance stations in "Rotterdam" now such that the travel time to each building from the closest ambulance station will be less than 14 minutes in the future?',
+#'What should be the location for each windmill of this windfarm now so that the sum of the amount of energy for each windmill of this windfarm will be maximal in the future?'
 ]
 
 
--- test_script.py
+++ test_script.py
@@ -1,1 +1,81 @@
-#testing file for commit and push
+import os
+from lark import Lark
+from lark import tree
+
+#grammar
+footer = r'''
+%import common.ESCAPED_STRING -> STRING
+%import common.SIGNED_NUMBER -> NUMBER
+%import common.WS
+%ignore WS
+'''
+
+spatialExperimentGrammar = r'''
+    spexperiment: measure (control)* ("in" spatialextent)?
+    measure : quantity | amount | concept
+    condmodifier : "the closest" | "the smallest" | "this" | "maximum" | "minimum"
+    control : (("for" | "from" | "to" | "of" | "between") ("each")? spexperiment) | condcontrol
+    condcontrol : spr onec | compr value | "with" optimal quantified simpleamount
+    amount :  simpleamount  |  relamount
+    simpleamount : ("amount of")? ("(")? concept (")")?
+    relamount : ("amount of")? ("(")? spexperiment (")")?
+    concept : condmodifier? (onec | twoc)
+    onec : object ("s")? | event ("s")? | stuff | space | time | onec "and" onec
+    twoc : onec "pair" | "pair of" onec
+    time : "time" | "travel time"
+    space : "space" |"location" | "height" | "distance" | STRING
+    spr : "within" | "touching" | "away from" | "west of" | STRING
+    compr : "larger than" | "less than" | "equal to" | "changed to" | "below" | STRING
+    quantity : quantified simpleamount | aggregated relamount
+    quantified : intensive | extensive
+    intensive :  "proportional" | "density of" ("the")? | "normalized"
+    optimal : "maximal" | "minimal"
+    aggregated :  "averaged" |  optimal | "sum of" ("the")?
+    extensive : "quantified" | "capacity of" ("the")? | "production of" ("the")?
+    object : "lifestock" | "place" | "building" | "city" | "neighborhood" | "hospital" | "inhabitant" | "windmill" | "windfarm" | ("ethanol")? "consumer" | ("ethanol")? "producer" | "ambulance station" | "road intersection" | "language group" | "route" | "sensor location"| "the world’s economy"|STRING
+    stuff :  "rain" | "soil"| "water" | "air pressure" | "noise" | "temperature" | "green" | "landcover" | "health" |  "energy"| "ethanol" | "cost" | "tax" | "CO2 emissions" | "NO2" | "PM10" | "PM2.5" | "gas extraction" | "Texelhopper busstops" | "road speeds" | STRING
+    event : "trip" | "period" | "earthquake" | "road accident" | "event" | STRING
+    spatialextent : STRING
+    value : NUMBER unit | "infinite" unit | STRING  | NUMBER | relativechange
+    unit : "minutes" | "kilometers" | "meters" | "R/l" | "liters" | "C" | "µg/m³" |
+    relativechange : ("increased"|"decreased"|"doubled"|"halved") ("by" NUMBER ("percent")?)?
+    '''
+
+questionGrammar = spatialExperimentGrammar + r'''
+    question : (contemporary | prediction | retrodiction | projection | retrojection) ("?")?
+    factualcondition : spexperiment ("is"|"are"|"was"|"were"|"to be"|"being") ("such and such"| optimal | compr value| value | STRING)  contemporaryreference
+    counterfactualcondition : spexperiment ("was"|"were") ("such and such"| optimal | compr value| value | STRING) contemporaryreference
+    projectedcondition : spexperiment ("will be"|"being") ("such and such"| optimal | compr value | value | STRING) futurereference
+    statisticalmodel : spexperiment contemporaryreference
+    transformationmodel : spexperiment contemporaryreference "given that" ("the")?  factualcondition
+    contemporary : "What" ("is"|"are") ("the")? (statisticalmodel|transformationmodel)
+    prediction : "What" "will be" ("the")? spexperiment futurereference "given that" ("the")?  factualcondition
+    retrodiction : "What" "could have been" ("the")? spexperiment pastreference ("given that" | "causing") ("the")? factualcondition
+    projection : "What" "would be" ("the")? spexperiment futurereference ("if"|"when") ("the")? counterfactualcondition
+    retrojection : "What" "should" ("have")? ("be"|"been") ("the")? spexperiment contemporaryreference ("so"|"such") "that" ("the")? projectedcondition
+    contemporaryreference : ("starting")? ("now" | "currently" | "at present" | "today" | "from now on" | "this summer" | "at the end of the African humid period")
+    pastreference : ("starting")? ("earlier" | "in the past" | NUMBER "years ago" | "last week" | "yesterday")
+    futurereference : ("starting")? ("in the future" | "later" | "in 2030" | "tomorrow" | "from now on" | "in 20 years" |"this summer")
+    '''
+
+#build parser
+parser = Lark(questionGrammar + footer, parser='earley', start='question', keep_all_tokens=True)
+
+#question
+#question = 'What should be the amount of green for each neighborhood in "Amsterdam" now so that the proportional amount of NO2 will be below 20 µg/m³ this summer?'
+#question = 'What would be the averaged amount of travel time between Texelhopper busstops in "Texel" in the future if the road speeds were increased by 20 percent today?'
+question = 'What would be the proportional amount of earthquakes for each location in "Groningen" in 2030 if the amount of gas extraction were halved today?'
+
+#parser
+try:
+    parsed_tree = parser.parse(question)
+    print("\nParsed successfully Parse tree:\n")
+    print(parsed_tree.pretty())
+    output_folder = "parseTreesCasper"
+
+    out_file = os.path.join(output_folder, "parse_tree.png")
+    tree.pydot__tree_to_png(parsed_tree, out_file)
+
+except Exception as e:
+    print("\n Parsing failed.")
+    print(e)
